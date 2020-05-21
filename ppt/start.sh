8d9290df9004f2404ad20eff0fb2168dbf8eabf9
Kevin Lewin <klewin@fusionstorm.com>
2020-05-21 16:02:29 -0400

    fix browser message

--- ppt/start.sh
+++ ppt/start.sh
@@ -6,6 +6,8 @@ fi
 
 rm /tmp/.X0-lock &>/dev/null || true
 
+rm -rf /root/.config/chromium
+
 export DISPLAY=:0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 echo "Starting X in 2 seconds"
